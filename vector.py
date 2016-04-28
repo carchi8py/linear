fe5f8a5f0c8aee7bbcd709faef557cc562cd1cc1
Chris Archibald <carchi8py@gmail.com>
2016-04-27 18:17:29 -0700

    Added Plus, Minus and Scalar mutiplication

--- vector.py
+++ vector.py
@@ -13,6 +13,32 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
+    def plus(self, v):
+        """
+        Add 2 vectors together
+        """
+        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        #another way to do this
+        #new_coordinates =[]
+        #n = len(self.coordinates)
+        #for i in range(n)
+        #   new_coordinates.append(self.coordinates[i], v.coordinates[i])
+        return Vector(new_coordinates)
+
+    def minus(self, v):
+        """
+        Minus 2 vectors together
+        """
+        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def times_scalar(self, c):
+        """
+        Mulipiles a vector by a scalar
+        """
+        new_coordinates = [c*x for x in self.coordinates]
+        return Vector(new_coordinates)
+
     #Print the vector
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
